ba0c68ee7da4b23f971b2b004fd6be66e9343230
Hassan Ahmed <107794603+hassanramzan11@users.noreply.github.com>
2025-02-01 12:25:00 +0500

    Video 1,2,3 covered in Day1
    Understand the concept about Java script
    In 1 video Introduction to JS.
    In 2 video how JS Works and what is Excecution Context & Call Stack.
    In 3 video what is Hoisting in JS.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var x=7 
+
+function getname1(){
+    console.log("Hassan Ahmed from getname1")
+
+}
+
+var getname2 = () => {
+    console.log("Hassan Ahmed from getname2")
+}
+
+
+getname1()
+getname2()
+
+console.log(x) 
